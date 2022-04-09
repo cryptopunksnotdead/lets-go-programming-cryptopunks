c1a5e6bcf78a95553d69490d0a099da5359d9fca
Gerald Bauer <gerald.bauer@gmail.com>
2022-04-09 12:46:04 +0200

    up

---
+++ punkverse/README.md
@@ -0,0 +1,67 @@
+# punkverse Package
+
+Generate (Pixel) Punk Images Using Built-In Archetypes and Attributes
+
+
+Example:
+
+
+``` go
+package main
+
+
+import (
+	"github.com/cryptopunksnotdead/lets-go-programming-cryptopunks/punkverse"
+)
+
+
+func main() {
+  punk := punkverse.Generate( "Female 2", "Earring", "Blonde Bob", "Green Eye Shadow" )
+	punk.Save( "./punk0.png" )
+	punk.Zoom( 10 ).Save( "./punk0@10x.png" )
+	punk.Background( "#60A4F7" ).Mirror().Zoom( 10 ).Save( "./phunk0@10x.png" )
+
+	punk = punkverse.Generate( "Male 1", "Smile", "Mohawk" )
+	punk.Save( "./punk1.png" )
+	punk.Zoom( 10 ).Save( "./punk1@10x.png" )
+	punk.Background( "#60A4F7" ).Mirror().Zoom( 10 ).Save( "./phunk1@10x.png" )
+
+	punk = punkverse.Generate( "Alien", "Headband" )
+	punk.Save( "./alien.png" )
+	punk.Zoom( 10 ).Save( "./alien@10x.png" )
+
+	punk = punkverse.Generate( "Zombie", "Crazy Hair" )
+	punk.Save( "./zombie.png" )
+	punk.Zoom( 10 ).Save( "./zombie@10x.png" )
+
+
+	///
+	//  try "raw" attribute images
+	female2 := punkverse.Find( "Female 2" )
+  female2.Save( "./female2.png" )
+  female2.Zoom( 10 ).Save( "./female2@10x.png" )
+
+	male1 := punkverse.Find( "Male 1" )
+	male1.Save( "./male1.png" )
+	male1.Zoom( 10 ).Save( "./male1@10x.png" )
+
+  tophatM := punkverse.Find( "Top Hat", punkverse.M )
+	tophatM.Save( "./tophat_(m).png" )
+	tophatM.Zoom( 10 ).Save( "./tophat_(m)@10x.png" )
+
+	crazyhairM := punkverse.Find( "Crazy Hair", punkverse.M )
+	crazyhairM.Save( "./crazyhair_(m).png" )
+	crazyhairM.Zoom( 10 ).Save( "./crazyhair_(m)@10x.png" )
+
+  crazyhairF := punkverse.Find( "Crazy Hair", punkverse.F )
+	crazyhairF.Save( "./crazyhair_(f).png" )
+	crazyhairF.Zoom( 10 ).Save( "./crazyhair_(f)@10x.png" )
+}
+```
+
+
+
+
+## Questions? Comments?
+
+Yes, you can. Post them on the [CryptoPunksDev reddit](https://old.reddit.com/r/CryptoPunksDev). Thanks.
---
+++ punkverse/image.go
@@ -0,0 +1,117 @@
+package punkverse
+
+import (
+  // "fmt"
+	"strings"
+	"regexp"
+	"log"
+
+	"github.com/pixelartexchange/artbase.server/pixelart"
+)
+
+
+
+// allow (ignore):
+//    space ( ),
+//    underscore (_),
+//    dash (-)
+var normalizeRegexp = regexp.MustCompile( "[ \t_-]" )
+
+func normalize( str string ) string {
+    str = strings.ToLower( str )
+		str = normalizeRegexp.ReplaceAllString( str, "" )
+		return str
+}
+
+
+/////
+//  todo/check:
+//     find/use a different (more idomatic?) interface to lookup attribute (images)
+//      - why? why not?
+//
+//    punkverse.Find( "Alien" )
+//
+//    punkverse.Find( "Headband", punkverse.MALE )
+//    punkverse.Find( "Headband", punkverse.M )
+//    punkverse.Find( "Headband", "m" )
+//
+//    punkverse.Find( "Headband", punkverse.FEMALE )
+//    punkverse.Find( "Headband", punkverse.F )
+//    punkverse.Find( "Headband", "f" )
+
+const (
+	 MALE   = "m"
+	 FEMALE = "f"
+	 M      = "m"
+	 F      = "f"
+)
+
+func Find( q string, opts... string  ) *pixelart.Image {
+   key := normalize( q )
+
+   for i, opt := range opts {
+     if i > 0 {
+			 // note: only zero or one option allowed for now; sorry
+			 log.Fatalf( "[punkverse.Find] ERROR - only zero or one option allowed for punkverse.Find; got >%v<", opts )
+		 }
+
+		 if opt == "m" {
+			 key = key + "_(m)"
+		 } else if opt == "f" {
+			 key = key + "_(f)"
+		 } else {
+			 log.Fatalf( "[punkverse.Find] ERROR - unknown option >%v<for punkverse.Find", opt )
+		 }
+	 }
+
+	 if pix, ok := pixmap[ key ]; ok {
+      return pix
+	 } else {
+      // panic - attribute not found !!
+			log.Fatalf( "[punkverse.Find] ERROR - attribute not found for >%s< using lookup key >%s<", q, key )
+			return nil
+	 }
+}
+
+
+func Generate( values ...string ) *pixelart.Image {
+  punkType       := values[0]
+  attributeNames := values[1:len(values)]
+
+	canvas := pixelart.NewImage( 24, 24 )
+
+  punkType = normalize( punkType )
+	punk := Find( punkType )
+  canvas.Paste( punk )
+
+	var m_or_f string
+	if strings.Index( punkType, "female" ) != -1 {
+		m_or_f = "f"
+	} else {
+		m_or_f = "m"
+	}
+
+
+	for _, attributeName := range attributeNames {
+		if attributeName == "" {   // skip empty attributes
+			continue
+		}
+
+		attributeName = normalize( attributeName )
+		attribute   := Find( attributeName, m_or_f )
+
+    canvas.Paste( attribute )
+	}
+
+	return canvas
+}
+
+
+
+
+/////
+// (more) convenience helper
+
+func makeImage24x24( pix []uint32 ) *pixelart.Image {
+  return pixelart.MakeImage( pix, &pixelart.Point{ 24,24 })
+}
